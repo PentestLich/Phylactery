b9a817187929d61368ef0d6f63b147e304c5d1a3
PentestLich <pt.ff.25@gmail.com>
2025-01-17 22:58:00 -0500

    Create setup-aliases.sh

---
+++ nmap/setup-aliases.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Define aliases for scripts
+alias nmapscan='curl -s https://raw.githubusercontent.com/PentestLich/Phylactery/refs/heads/main/nmap/nmap-scan.sh | bash -s'
+
+# Optional: Print a confirmation message
+echo "Aliases have been set up! Use the following commands:"
+echo "  nmapscan <target>"
