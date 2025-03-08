3297c265ec8180b22a3602e805baffe225686446
PentestLich <pt.ff.25@gmail.com>
2025-03-08 02:09:14 -0500

    Update setup-aliases.sh

--- setup-aliases.sh
+++ setup-aliases.sh
@@ -2,12 +2,7 @@
 
 # Define aliases for scripts
 alias updatesystem='curl -s https://raw.githubusercontent.com/PentestLich/Phylactery/main/general/update | bash'
-alias nmapscan='curl -s https://raw.githubusercontent.com/PentestLich/Phylactery/refs/heads/main/nmap/nmap-scan.sh | bash -s'
-alias gobusterscan='curl -s https://raw.githubusercontent.com/PentestLich/Phylactery/refs/heads/main/gobuster/gobuster-scan.sh | bash -s'
-
 
 # Optional: Print a confirmation message
 echo "Aliases have been set up! Use the following commands:"
 echo "  updatesystem"
-echo "  nmapscan <target>"
-echo "  gobusterscan <target> [wordlist]"
